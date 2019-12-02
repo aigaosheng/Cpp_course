fbdced9d92d8d875c82c290fcda5d94aea799d63
gao <goseng123@gmail.com>
2019-12-02 23:45:07 +0800

    update

--- noi/pyutils/mevi.py
+++ noi/pyutils/mevi.py
@@ -6,33 +6,27 @@ import matplotlib.pyplot as plt
 
 
 with open('/home/gao/Work/Cpp_course/noi/tree/fenwickTree.txt') as fi:
-    lns = map(lambda x: x.strip('\n'), fi.readlines())
+    lns = list(map(lambda x: x.strip('\n'), fi.readlines()))
     #print(lns)
-    nn = 4#len(lns)
-    g = Graph()
-    g.add_vertices(range(1, nn + 1))
-    layout = g.layout("kk")
-    plx = plot(g, layout = layout)
-    for imgid, ln in enumerate(lns):
-        nd = ln.strip('\n').split('img')
-        in_id, in_v = nd[0].split(':')#current input id, value
-        for i_v in nd[1].split('*'):
-            #print(i_v)
-            if len(i_v) == 0:
-                continue
-            n_v = i_v.split(',')
-            bit = n_v[0].split(':')
-            #ed = map(lambda x:(int(x[0],int(x[1]))),map(lambda x:x.split(':'), n_v[1:]))
-            ed = filter(lambda x:len(x)>0, map(lambda x:x.split(':'), n_v[1:]))
-            ed = map(lambda x: (int(x[0]), int(x[1])), ed)
-            ed = filter(lambda x: len(x)==2, ed)
-            #print(list(ed))
-
-            g.add_edges(ed)
-            #layout = g.layout("kk")
-            #plx = plot(g, layout = layout, opacity=0.1)
-            plx.add(g)
-            plx.show()
+    nn = len(lns) - 1
+    #g = Graph()
+    #g.add_vertices(range(1, nn + 1))
+    #layout = g.layout("tree")
+    #plot(g, layout = layout)
+    for ln in lns[1:]:
+        nd = list(map(lambda x:(int(x[0]), int(x[1])), map(lambda x:x.split(':'), ln.split(','))))
+        in_id, in_v = nd[0]#current input id, value
+        g = Graph()
+        g.add_vertex(0)
+        for k in range(1, nn):
+            g.add_vertices(k)
+            g.add_edges([(0, k)])
+        for bit_node, bit_v in nd[1:]:
+            if in_id != bit_node:
+                g.add_edges([(in_id, bit_node)])
+        layout = g.layout_reingold_tilford()
+        plot(g, layout = layout)#, opacity=0.1)
+            #plx.show()
 
         #plt.show()
         #plt.savefig('t{}.png'.format(imgid))
